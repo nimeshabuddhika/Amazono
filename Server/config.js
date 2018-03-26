eb94e3c3a3668c1c097fc87bb43aba901f34616c
Nimesha Buddhika <135071J@uom.lk>
2018-03-26 23:51:27 +0530

    conflit sloved

---
+++ Server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database : 'mongodb://localhost/amazono',
+    port : '3030'
+}
---
+++ Server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+mongoose.connect(config.database, (error) => {
+    if(error)
+        console.log(error)
+    else
+        console.log("Connection successful");
+})
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended :false}));
+app.use(morgan('dev'));
+app.use(cors());
+
+
+app.get('/',(req,res,next) => {
+    res.json({
+        user: "Nimesha"
+    });
+});
+
+app.listen(config.port, (err) => {
+    console.log("Amozono started... hi "+ config.port );
+});
