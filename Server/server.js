12d6e8049ec019f37589c8675c3528612552aa83
Nimesha Buddhika <135071J@uom.lk>
2018-04-29 15:26:17 +0530

    database connection is done

--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,29 @@
-const express = require('express'); //Web framework to create http routes
-const morgan = require('morgan');  //Middle ware to log http request
-const bodyParser = require('body-parser'); //data reader, NodeJs can not communicate with front end data
-const mongoose = require('mongoose'); //data base connector of  mongodb
+const express = require('express');         //Web framework to create http routes
+const morgan = require('morgan');           //Middle ware to log http request
+const bodyParser = require('body-parser');  //data reader, NodeJs can not communicate with front end data
+const mongoose = require('mongoose');       //data base connector of  mongodb
 
 const app = express();
+
+const config = require('./config');
+
+mongoose.connect(config.database, (err) =>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Connected to database')
+    }
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
 app.use(morgan('dev'));
+
+app.get('/',(req,res,next) => {
+    res.json({
+        'user':'Nimesha'
+    });
+})
 
 app.listen(3030, (err) => {
     console.log('Server started...');
